7d7cd2081588103fed5919d13c70e03c82ebdd8b
Jonathan Gruss <jonathangruss@hotmail.com>
2023-03-23 10:47:28 +0100

    bottom nav

--- frontend/components/BottomNav.js
+++ frontend/components/BottomNav.js
@@ -1,8 +1,21 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { FaHome, FaSearch, FaPlus, FaUser } from 'react-icons/fa';
+import { FaHome, FaPlus, FaTrophy, FaUser } from 'react-icons/fa';
 
 const BottomNav = () => {
+  return (
+    <nav className='fixed bottom-0 left-0 w-full h-16 bg-zinc-200 rounded-t-3xl pt-2'>
+      <div className='flex flex-row justify-around items-center w-full'>
+        <NavItem href='/' icon={<FaHome />} />
+        <NavItem href='/task' icon={<FaPlus />} />
+        <NavItem href='/leaderboard' icon={<FaTrophy />} />
+        <NavItem href='/profile' icon={<FaUser />} />
+      </div>
+    </nav>
+  );
+};
+
+const NavItem = ({ href, icon }) => {
   const router = useRouter();
 
   const isActive = (path) => {
@@ -10,32 +23,17 @@ const BottomNav = () => {
   };
 
   return (
-    <nav className='fixed bottom-0 left-0 w-full h-16 bg-white justify-around items-center'>
-      <Link href='/'>
-        <div className={`block text-center ${isActive('/')}`}>
-          <FaHome />
-          <div className='text-xs'>Home</div>
+    <div className=''>
+      <Link href={href}>
+        <div
+          className={`block text-center text-2xl ${isActive(
+            href
+          )} rounded-full p-3`}
+        >
+          {icon}
         </div>
       </Link>
-      <Link href='/search'>
-        <div className={`block text-center ${isActive('/search')}`}>
-          <FaSearch />
-          <div className='text-xs'>Search</div>
-        </div>
-      </Link>
-      <Link href='/add'>
-        <div className={`block text-center ${isActive('/add')}`}>
-          <FaPlus />
-          <div className='text-xs'>Add</div>
-        </div>
-      </Link>
-      <Link href='/profile'>
-        <div className={`block text-center ${isActive('/profile')}`}>
-          <FaUser />
-          <div className='text-xs'>Profile</div>
-        </div>
-      </Link>
-    </nav>
+    </div>
   );
 };
 
